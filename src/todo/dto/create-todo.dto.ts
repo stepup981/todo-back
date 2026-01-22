a553fa753428326aa4994d5deaa54194ebc435ad
Roman Berdnikov <berdnikov.roma@gmail.com>
2026-01-22 19:48:22 +0300

    написал декораторы-валидации на DTO

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreateTodoDto {
+  @IsString({ message: 'Название задачи должно являтся строкой' })
+  @IsNotEmpty({ message: 'Название задачи не должно быть пустым' })
   name: string;
+  @IsOptional()
+  @IsString({ message: 'Описание задачи должно являтся строкой' })
   description?: string;
 }
--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class UpdateTodoDto {
+  @IsString({ message: 'Название задачи должно являтся строкой' })
+  @IsNotEmpty({ message: 'Название задачи не должно быть пустым' })
   name: string;
+
+  @IsString({ message: 'Описание задачи должно являтся строкой' })
   description: string;
 }
